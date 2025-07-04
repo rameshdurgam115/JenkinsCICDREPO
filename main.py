7c379e08df99a3a77b5a98129b073455e27feb4e
Ramesh <rameshdurgam115.com>
2025-07-04 17:37:15 -0400

    Add Dockerfile and Jenkinsfile for CI/CD

--- main.py
+++ main.py
@@ -2,4 +2,4 @@ from fastapi import FastAPI
 app = FastAPI()
 @app.get("/")
 def read_root():
- return {"message": "Hello, Welcome to Jenkins"}
+ return {"message": "Hello, Welcome to Jenkins, CI/CD pipeline"}
